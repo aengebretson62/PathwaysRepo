09e4c4d0f866b098d52951abfeaa2ad0e8129595
Alec Engebretson <alec.engebretson@doane.edu>
2022-04-23 12:38:54 -0500

    listInterfaceCRUD working through CR

---
+++ listInterfaceCRUD/HourlyEmployee.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace listInterfaceCRUD
+{
+    class HourlyEmployee : Employee, IGetBonus
+    {
+        public double hourlyRate  // property
+            { get; set; }
+
+
+        public HourlyEmployee() : base()      // default constructor
+        {
+            hourlyRate =  0.0;
+        }
+
+        public HourlyEmployee(string newLastName, string newFirstName, string newEmployeeType, double newHourlyRate) : base (newLastName, newFirstName, newEmployeeType)       // another constructor
+        {
+            hourlyRate = newHourlyRate;
+        }
+
+        public double GetBonus() // interface method
+        {
+            return hourlyRate * 80;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " | Hourly rate: $" + hourlyRate + " | Bonus: $" + GetBonus();
+        }
+
+    }  // end class
+
+}  // end namespace
---
+++ listInterfaceCRUD/SalaryEmployee.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace listInterfaceCRUD
+{
+    class SalaryEmployee : Employee, IGetBonus
+    {
+        public double annualRate  // property
+            { get; set; }
+
+
+        public SalaryEmployee() : base()      // default constructor
+        {
+            annualRate =  0.0;
+        }
+
+        public SalaryEmployee(string newLastName, string newFirstName, string newEmployeeType, double newAnnualRate) : base (newLastName, newFirstName, newEmployeeType)       // another constructor
+        {
+            annualRate = newAnnualRate;
+        }
+
+        public double GetBonus() // interface method
+        {
+            return annualRate * .1;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " | Annual salary: $" + annualRate + " | Bonus: $" + GetBonus();
+        }
+
+    }  // end class
+
+}  // end namespace
